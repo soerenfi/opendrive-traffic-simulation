9ed27452aba4c9ec771c7b3d7fa2e585ed5267d4
sofinde <soeren.findeisen@daimler.com>
2021-07-09 16:28:21 +0200

    gitignore

--- build/thirdparty/tinyxml2/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /lhome/sofinde/udacity/capstone/thirdparty/tinyxml2
-# Build directory: /lhome/sofinde/udacity/capstone/build/thirdparty/tinyxml2
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(xmltest "/lhome/sofinde/udacity/capstone/build/thirdparty/tinyxml2/xmltest")
-set_tests_properties(xmltest PROPERTIES  PASS_REGULAR_EXPRESSION ", Fail 0" WORKING_DIRECTORY "/lhome/sofinde/udacity/capstone/thirdparty/tinyxml2" _BACKTRACE_TRIPLES "/lhome/sofinde/udacity/capstone/thirdparty/tinyxml2/CMakeLists.txt;48;add_test;/lhome/sofinde/udacity/capstone/thirdparty/tinyxml2/CMakeLists.txt;0;")
